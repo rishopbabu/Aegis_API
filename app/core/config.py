c1f934dcad68e29ce1406028ccdf07ef8edfc745
Vani Kambam <kambhamvani2020@gmail.com>
2026-05-26 12:42:17 +0000

    Initial commit

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    app_name: str
+
+    database_url: str
+    redis_url: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+
+settings = Settings()
